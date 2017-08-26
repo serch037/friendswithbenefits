ac9b0c706bd17f3de0c3f0cfdd20bfbd38af9b6a
Sergio Ugalde Marcano <serch037@gmail.com>
2017-08-26 16:14:36 -0500

    Login route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
+import {HashRouter, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import login from './components/login'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <HashRouter>
+        <div>
+          <h1>Main</h1>
+          <Route exact path="/" component={login}/>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </HashRouter>
     );
   }
 }
